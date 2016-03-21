b067abb7a03446add28d823c7d9e5f7c749c67f2
Bizmonger <scott.nimrod@bizmonger.net>
2016-03-21 17:28:50 -0400

    Added a property test

--- Client/ViewModel.fs
+++ Client/ViewModel.fs
@@ -11,10 +11,10 @@ type ViewModel() as self =
     inherit ViewModelBase()
 
     let rowCount = 6
-    let mutable grid = rowCount |> createGrid
-                                |> setCell { X=3; Y=1; State=Alive }
-                                |> setCell { X=3; Y=0; State=Alive }
-                                |> setCell { X=4; Y=1; State=Alive }
+    let grid = rowCount |> createGrid
+                        |> setCell { X=3; Y=1; State=Alive }
+                        |> setCell { X=3; Y=0; State=Alive }
+                        |> setCell { X=4; Y=1; State=Alive }
 
     let mutable _cells = grid |> Map.toSeq
                               |> Seq.map snd
---
+++ GameOfLife/PropertyTests.fs
@@ -0,0 +1,10 @@
+﻿module PropertyTests
+
+open FsCheck
+open Xunit
+open Model
+
+[<Fact>]
+let ``cell can't have more than nine eight neighbors`` () = 
+    Check.QuickThrowOnFailure <| fun xy -> xy |> getNeighbors
+                                              |> List.length < 9
