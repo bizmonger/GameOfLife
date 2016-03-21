79787c33cab0ced03ef897a21a67f99019300328
Bizmonger <scott.nimrod@bizmonger.net>
2016-03-21 17:30:02 -0400

    updates test name

--- GameOfLife/PropertyTests.fs
+++ GameOfLife/PropertyTests.fs
@@ -5,6 +5,6 @@ open Xunit
 open Model
 
 [<Fact>]
-let ``cell can't have more than nine eight neighbors`` () = 
+let ``cell can't have more than eight neighbors`` () = 
     Check.QuickThrowOnFailure <| fun xy -> xy |> getNeighbors
                                               |> List.length < 9
