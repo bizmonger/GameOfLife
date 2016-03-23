f667bed8ab9c978f28d034dadf0c1d41043c0482
Bizmonger <scott.nimrod@bizmonger.net>
2016-03-22 21:48:36 -0400

    Resolved UI bug

--- Client/ViewModel.fs
+++ Client/ViewModel.fs
@@ -6,6 +6,7 @@ open UILogic.Interaction
 open Model
 open System
 open System.Windows.Input
+open System.Collections.ObjectModel
 
 type ViewModel() as self =
     inherit ViewModelBase()
@@ -16,25 +17,77 @@ type ViewModel() as self =
                         |> setCell { X=3; Y=0; State=Alive }
                         |> setCell { X=4; Y=1; State=Alive }
 
-    let mutable _cells = grid |> Map.toSeq
-                              |> Seq.map snd
-                              |> Seq.toList
+    let mutable _cells = ObservableCollection<Cell>( grid |> Map.toSeq
+                                                          |> Seq.map snd
+                                                          |> Seq.toList )
     let cycleHandler _ = 
 
-        self.Items <- grid |> cycleThroughCells
-                           |> Map.toSeq
-                           |> Seq.map snd
-                           |> Seq.toList
+        self.Cells <- ObservableCollection<Cell>( grid |> cycleThroughCells
+                                                       |> Map.toSeq
+                                                       |> Seq.map snd
+                                                       |> Seq.toList )
 
-    member self.Play =
-        DelegateCommand ((fun _ -> let timer = createTimer 500 cycleHandler
-                                   do while true do
-                                      do Async.RunSynchronously timer), fun _ -> true) :> ICommand
+    let change _ = 
+
+        async
+            {
+                while true do
+                    do! Async.Sleep 500
+                    cycleHandler()
+            }
+        |> Async.Start
+
+    member self.Play = DelegateCommand (change, fun _ -> true) :> ICommand
+
     member self.N = rowCount
 
-    member self.Items
+    member self.Cells
         with get() = _cells 
         and set(value) =
             _cells <- value
 
-            base.NotifyPropertyChanged(<@ self.Items @>)
+            base.NotifyPropertyChanged(<@ self.Cells @>)
+
+//type ViewModel() as self = 
+//    inherit ViewModelBase()   
+//
+//    let rowCount = 6
+//    let mutable grid = rowCount |> createGrid
+//                                |> setCell { Y=2; X=1; State=Alive }
+//                                |> setCell { Y=2; X=2; State=Alive }
+//                                |> setCell { Y=3; X=2; State=Alive }
+//                                |> setCell { Y=3; X=3; State=Alive }
+//
+//    let mutable _cells = grid |> Map.toSeq
+//                              |> Seq.map snd
+//                              |> Seq.toList
+//
+//    let mutable count = 0
+//    let mutable isEnabled = true
+//
+//    let cycleHandler _ = 
+//        _cells <- grid |> cycleThroughCells
+//                       |> Map.toSeq
+//                       |> Seq.map snd
+//                       |> Seq.toList
+//        count <- count + 1
+//        self.NotifyPropertyChanged(<@ self.Cells @>)
+//        self.NotifyPropertyChanged(<@ self.Count @>)
+//
+//    let change _ = 
+//        isEnabled <- false
+//        self.NotifyPropertyChanged(<@ self.IsEnabled @>)
+//
+//        async
+//            {
+//                while true do
+//                    do! Async.Sleep 2000
+//                    cycleHandler()
+//            }
+//        |> Async.Start
+//
+//    member __.Play = DelegateCommand (change, fun _ -> isEnabled) :> ICommand
+//    member __.N = rowCount
+//    member __.Cells = _cells
+//    member __.Count = count
+//    member __.IsEnabled = isEnabled
--- GameOfLife/PropertyTests.fs
+++ GameOfLife/PropertyTests.fs
@@ -8,3 +8,9 @@ open Model
 let ``cell can't have more than eight neighbors`` () = 
     Check.QuickThrowOnFailure <| fun xy -> xy |> getNeighbors
                                               |> List.length < 9
+[<Fact>]
+let ``number of cells in grid equals rowcount squared`` () =
+    Check.QuickThrowOnFailure <| 
+            fun rowCount -> rowCount >= 0 ==> (rowCount |> createGrid
+                                                        |> Map.toList
+                                                        |> List.length = rowCount * rowCount)
